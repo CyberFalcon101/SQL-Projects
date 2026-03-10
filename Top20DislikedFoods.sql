6cdf9102b3826c95e1f142cb31c115d95129d16d
Cyber Falcon <afifaamin@gmail.com>
2026-03-10 15:45:33 +1100

    Create Top20DislikedFoods.sql

---
+++ Top20DislikedFoods.sql
@@ -0,0 +1,27 @@
+-- Copyright CyberFalcon101. Forks are allowed as long as the author is credited.
+CREATE TABLE Food(
+RANK INTEGER,
+FOOD TEXT,
+CATEGORY TEXT
+);
+INSERT INTO Food VALUES(1,'Liver','Meat');
+INSERT INTO Food VALUES(2,'Anchovy','Seafood');
+INSERT INTO Food VALUES(3,'Blue Cheese','Dairy');
+INSERT INTO Food VALUES(4,'Durian','Fruit');
+INSERT INTO Food VALUES(5,'Oysters','Seafood');
+INSERT INTO Food VALUES(6,'Brussels Sprouts','Vegetable');
+INSERT INTO Food VALUES(7,'Cilantro','Vegetable');
+INSERT INTO Food VALUES(8, 'Okra','Vegetable');
+INSERT INTO Food VALUES(9,'Haggis','Meat');
+INSERT INTO Food VALUES(10,'Kale','Vegetable');
+INSERT INTO Food VALUES(11,'Escargot','Meat');
+INSERT INTO Food VALUES(12,'Gorgonzola','Dairy');
+INSERT INTO Food VALUES(13,'Bitter Melon','Vegetable');
+INSERT INTO Food VALUES(14,'Sardines','Seafood');
+INSERT INTO Food VALUES(15,'Tripe','Meat');
+INSERT INTO Food VALUES(16,'Limburger Cheese','Dairy');
+INSERT INTO Food VALUES(17,'Gefilte Fish','Seafood');
+INSERT INTO Food VALUES(18,'Jellied Eels','Seafood');
+INSERT INTO Food VALUES(19,'Fermented Herring','Seafood');
+INSERT INTO Food VALUES(20,'Fugu','Seafood');
+SELECT * FROM Food
