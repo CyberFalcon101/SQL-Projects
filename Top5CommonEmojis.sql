80f6c4d2251026921a250b6f10d0e2c6d5b62f39
Cyber Falcon <afifaamin@gmail.com>
2026-03-10 15:44:35 +1100

    Create Top5CommonEmojis.sql

---
+++ Top5CommonEmojis.sql
@@ -0,0 +1,32 @@
+--#COPYRIGHT CyberFalcon101. Forks are allowed as long as the author is credited.
+create table EmojiTB (
+RANK INTEGER,
+EMOJI TEXT,
+DESCRIPTION TEXT
+);
+INSERT INTO EmojiTB VALUES(1,'😂','Laughing');
+INSERT INTO EmojiTB VALUES(2,'🫠','Melting');
+INSERT INTO EmojiTB VALUES(3,'😉','Wink');
+INSERT INTO EmojiTB VALUES(4,'☺️','Warm-smile');
+INSERT INTO EmojiTB VALUES(5,'🥲','Happy-cry');
+INSERT INTO EmojiTB VALUES(6,'😋','Yum');
+INSERT INTO EmojiTB VALUES(7,'🫣','Peeking');
+INSERT INTO EmojiTB VALUES(8,'🤫','Shh');
+INSERT INTO EmojiTB VALUES(9,'🤔','Thinking-face');
+INSERT INTO EmojiTB VALUES(10,'🫡','Salute');
+INSERT INTO EmojiTB VALUES(11,'🤨','Raised-eyebrow');
+INSERT INTO EmojiTB VALUES(12,'😮','Mouth-open');
+INSERT INTO EmojiTB VALUES(13,'🫨','Shaking-face');
+INSERT INTO EmojiTB VALUES(14,'🙂‍↔️','Head-shake');
+INSERT INTO EmojiTB VALUES(15,'🙂‍↕️','Head-nod');
+INSERT INTO EmojiTB VALUES(16,'🥵','Sweat-face');
+INSERT INTO EmojiTB VALUES(17,'🥳','Partying-face');
+INSERT INTO EmojiTB VALUES(18,'🤓','Glasses-face');
+INSERT INTO EmojiTB VALUES(19,'🥺','Pleading');
+INSERT INTO EmojiTB VALUES(20,'🥹','Holding-back-tears');
+INSERT INTO EmojiTB VALUES(21,'😭','Loudly-crying');
+INSERT INTO EmojiTB VALUES(22,'😈','Imp-smile');
+INSERT INTO EmojiTB VALUES(23,'💀','Skull');
+INSERT INTO EmojiTB VALUES(24,'🤡','Clown');
+INSERT INTO EmojiTB VALUES(25,'🤖','Robot');
+SELECT * FROM EmojiTB;
