292d39191e2aaf07c6254be609f4b6e97203c9f1
Cyber Falcon <afifaamin@gmail.com>
2026-03-10 15:46:35 +1100

    Create Top30StrangeCombos.sql

---
+++ Top30StrangeCombos.sql
@@ -0,0 +1,35 @@
+-- Copyright CyberFalcon101. Forks are allowed as long as the author is credited.
+create table combos(
+RANK integer,
+COMBO text
+);
+insert into combos values(1,'Peanut Butter and Pickles');
+insert into combos values(2,'Chips and Ice-cream');
+insert into combos values(3,'Cheese and Apple Pie');
+insert into combos values(4,'Banana and Mayonnaise Sandwich');
+insert into combos values(5,'Strawberries and Vinegar');
+insert into combos values(6,'Pizza and Honey');
+insert into combos values(7,'Avocado and Chocolate');
+insert into combos values(8,'Grapes and Cheese');
+insert into combos values(9,'Salt and Watermelon');
+insert into combos values(10,'Mango and Chilli Powder');
+insert into combos values(11,'Ice-cream and Olive Oil');
+insert into combos values(12,'Cheese and Jelly Sandwiches');
+insert into combos values(13,'Salt and Apples');
+insert into combos values(14,'Peanut Butter and Pickles');
+insert into combos values(15,'Strawberries and Cheese');
+insert into combos values(16,'Cucumber and Honey');
+insert into combos values(17,'Sugar and Tomato');
+insert into combos values(18,'Vinegar and Ice-cream');
+insert into combos values(19,'Avocado and Sugar');
+insert into combos values(20,'Tuna and Olives');
+insert into combos values(21,'Peanut Butter and Celery');
+insert into combos values(22,'Watermelon and Vinegar');
+insert into combos values(23,'Cream Cheese and Olives');
+insert into combos values(24,'Honey and Pizza');
+insert into combos values(25,'Cheese and Mustard');
+insert into combos values(27,'Ice-cream and Soy Sauce');
+insert into combos values(28,'Peanut Butter and Eggs');
+insert into combos values(29,'Coffee and Butter');
+insert into combos values(30,'Zucchini and Cinnamon');
+select * from combos
