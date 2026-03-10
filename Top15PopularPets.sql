2dc1c3e3086b5bea8a72971fe7a45510de0fb23c
Cyber Falcon <afifaamin@gmail.com>
2026-03-10 15:47:27 +1100

    Create Top15PopularPets.sql

---
+++ Top15PopularPets.sql
@@ -0,0 +1,22 @@
+-- Copyright CyberFalcon101. Forks are allowed as long as the author is credited.
+create table pets(
+RANK integer,
+ANIMAL text,
+'' text
+);
+insert into pets values (1,'Dogs','🐕');
+insert into pets values (2,'Cats','🐈');
+insert into pets values (3,'Fish','🐟');
+insert into pets values (4,'Birds','🐦');
+insert into pets values (5,'Hamsters','🐹');
+insert into pets values (6,'Guinea Pigs','❔');
+insert into pets values (7,'Rabbits','🐇');
+insert into pets values (8,'Turtles','🐢');
+insert into pets values (9,'Ferrets','❓');
+insert into pets values (10,'Lizards','🦎');
+insert into pets values (11,'Snakes','🐍');
+insert into pets values (12,'Horses','🐴');
+insert into pets values (13,'Mice','🐁');
+insert into pets values (14,'Frogs','🐸');
+insert into pets values (15,'Spiders','🕷️');
+select * from pets
